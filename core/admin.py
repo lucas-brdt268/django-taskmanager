2eb926566276d603c40b0236e06104edbc0d1c8a
lucas-brdt268 <lucas.brdt268@gmail.com>
2025-06-29 09:49:23 +0300

    Complete admin part

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Task
 
 # Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'created_at', 'completed')
+    search_fields = ('title', 'description')
+    list_filter = ('completed', 'created_at')
+    ordering = ('-created_at',)
